58f4d2d68127e397d9f6d7457a10a35616d143bf
Fareedah James <fjames0578@gmail.com>
2024-01-31 18:02:37 -0700

    Update helloWorld.sol
    LTV hello world assignment

--- helloWorld.sol
+++ helloWorld.sol
@@ -1,1 +1,27 @@
 
+   // SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0; //version
+
+
+    contract HelloWorld { // Defination of the contract
+            //parameter of the HelloWorld 
+            
+       string public greeting; // contract creation 
+
+        constructor() {
+        
+        // assign greeting to the string "Hello World"
+            greeting = "Hello World";
+    }
+    
+        // Getter function returns the greeting "Hello World"
+
+        function createHelloWorld() public view returns (string memory) {
+            return greeting; // returns "Hello World"
+        }
+
+
+
+    }
+
+   
